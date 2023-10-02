e8b1a05c39192d40a89be14b18209a76d21b5cb7
matheusscode <matheussdev3@gmail.com>
2023-10-02 16:50:59 -0400

    feat: add MongoGetUsersRepository

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
+import { GetUsersController } from "./controllers/get-users/get-users";
 import express from "express";
 import { config } from "dotenv";
 
 config();
 
 const app = express();
-
 const port = process.env.PORT || 3030;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/users", async (req, res) => {
+  const mongoGetUsersRepository = new MongoGetUsersRepository();
+  const getUsersController = new GetUsersController(mongoGetUsersRepository);
+
+  const { body, statusCode } = await getUsersController.handle();
+
+  res.send(body).status(statusCode);
 });
 
 app.listen(port, () => console.log(`Server is running in port ${port} 🚀`));
